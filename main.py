c2f943ff89ba0e7970368875fc084f61acc305c0
Shachar <shachar.resisi@gmail.com>
2025-09-18 19:49:47 +0300

    thermal cam

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import os
 
 
 from optical_camera.gesture_detection import init_gesture_recognizer, is_wave_gesture
-
+from thermal_camera.adafruit_cam import init_thermal_camera
+import config
 
 gestures_recognizer = None
+thermal_camera = None
 
 
 def init():
     model_path = os.path.abspath('./optical_camera/gesture_recognizer.task')
     gestures_recognizer = init_gesture_recognizer(model_path)
+    thermal_camera = init_thermal_camera(config.THERMAL_FRAME_RATE, (config.THERMAL_H, config.THERMAL_W))
 
 
 def main():
