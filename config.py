2fe42b8085e1c39fba53bb0a692ab35f0651eab1
marianas <marianas@nexite.io>
2025-09-19 11:12:23 +0300

    NOT TESTED! (1) Integrated main flow (2) The capture cam has grace frames (3) Added state

--- config.py
+++ config.py
@@ -32,9 +32,10 @@ THERMAL_MIN_TEMP = 20
 OPTICAL_CAM_ID = 0
 OPTICAL_H = 480 
 OPTICAL_W = 640 
-OPTICAL_YOLO_WH = 320  # width/height for YOLO model input size
+OPTICAL_FAILED_CAPTURE_COUNTER_FOR_CRASH = 5
 
-# PPL detection 
+# PPL detection YOLO
+OPTICAL_YOLO_WH = 320  # width/height for YOLO model input size
 OPTICAL_YOLO_MODEL = "yolo11n.pt"  
 OPTICAL_IS_PERSON_YOLO_THR = 0.5  # confidence threshold for person detection
 
--- main.py
+++ main.py
@@ -5,38 +5,40 @@ import config
 from stabilizer import init_stabilizer, update_buffer, get_stable_boxes
 from optical_camera.gesture_detection import init_gesture_recognizer, is_wave_gesture
 from optical_camera.capture_frame_cv2 import capture_frame
+from optical_camera.YOLO_detect_ppl import detect_people
 from thermal_camera.adafruit_cam import init_thermal_camera, get_max_temp
 
 
 gestures_recognizer = None
 thermal_camera = None
 history = None
-dState = dict()
+d_state = {'isInitalized': False}
 
 
 def init():
+    if d_state['isInitalized']:
+        return d_state
     model_path = os.path.abspath('./optical_camera/gesture_recognizer.task')
     gestures_recognizer = init_gesture_recognizer(model_path)
     thermal_camera = init_thermal_camera(config.THERMAL_FRAME_RATE, (config.THERMAL_H, config.THERMAL_W))
     history = init_stabilizer(config.STABILIZER_N_FRAMES)
-    dState['isInitalized'] = True
+    d_state['isInitalized'] = True
 
-    return dState
+    return d_state
     
 
 
 def main():
     # init HW
-    dState = init()
+    d_state = init()
     # main loop:
     while True:
         # capture image
-        frame = capture_frame_cv2()
+        frame, d_state = capture_frame(d_state)
 
         # detection + tracking
-        detected_boxes, detected_centers, annotated_frame, dState = detect_people(frame, state=dState, return_annotated=True)
+        detected_boxes, detected_centers, annotated_frame, d_state = detect_people(frame, state=d_state, return_annotated=True)
         yolo_res = None
-        detected_boxes = [r.box.xyxy[0].cpu().numpy() for r in yolo_res if r.cls == 0 and r.conf > config.OPTICAL_IS_PERSON_YOLO_THR]  # only persons
         history = update_buffer(history, detected_boxes)
         boxes_to_consider = get_stable_boxes(history, detected_boxes, config.STABILIZER_M_FRAMES)
         
